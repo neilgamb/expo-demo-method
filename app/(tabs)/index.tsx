a0c234f17f9278e52b225838d76cb08840e561d8
Neilson Gamble <neilgamb@gmail.com>
2023-07-12 19:07:50 -0600

    03_camera_base

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,19 +6,19 @@ export default function TabOneScreen() {
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [type, setType] = useState(CameraType.back);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await Camera.requestCameraPermissionsAsync();
+  //     setHasPermission(status === "granted");
+  //   })();
+  // }, []);
 
-  if (hasPermission === null) {
-    return <View />;
-  }
-
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+  if (hasPermission === false || hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>No access to camera</Text>
+      </View>
+    );
   }
 
   return (
